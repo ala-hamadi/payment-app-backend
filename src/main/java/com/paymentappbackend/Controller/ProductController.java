f6b3fe288444c024472c600f0fddb5ed00bf0dc0
ala-hamadi <ala.hamadi.01@gmail.com>
2023-11-10 17:51:48 +0100

    fix: product api security

--- src/main/java/com/paymentappbackend/Controller/ProductController.java
+++ src/main/java/com/paymentappbackend/Controller/ProductController.java
@@ -6,12 +6,14 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.paymentappbackend.Entity.Products;
 import com.paymentappbackend.Service.ProductService;
 
 @RestController
+@RequestMapping("/oauth")
 public class ProductController {
 
   @Autowired
