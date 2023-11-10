fa7c3b98f7ea24f13ea9ccdf3abaa1987b7d668e
ala-hamadi <ala.hamadi.01@gmail.com>
2023-11-10 17:01:36 +0100

    feat: implement product api

--- src/main/java/com/paymentappbackend/Controller/ProductController.java
+++ src/main/java/com/paymentappbackend/Controller/ProductController.java
@@ -1,12 +1,29 @@
 package com.paymentappbackend.Controller;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.paymentappbackend.Entity.Products;
 import com.paymentappbackend.Service.ProductService;
 
 @RestController
 public class ProductController {
+
   @Autowired
   private ProductService productService;
+
+  @GetMapping("/getAllProducts")
+  public List<Products> getAllProducts() {
+    return productService.getAllProducts();
+  }
+
+  @GetMapping("/getProductById/{id}")
+  public Optional<Products> getProductById(@PathVariable(value = "id") int id) {
+    return productService.getProductById(id);
+  }
 }
--- src/main/java/com/paymentappbackend/Service/ProductService.java
+++ src/main/java/com/paymentappbackend/Service/ProductService.java
@@ -1,12 +1,29 @@
 package com.paymentappbackend.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.paymentappbackend.Entity.Products;
 import com.paymentappbackend.Repository.ProductRepository;
 
 @Service
 public class ProductService {
+
   @Autowired
   private ProductRepository productRepository;
+
+  public List<Products> getAllProducts() {
+    return productRepository.findAll();
+  }
+
+  public Optional<Products> getProductById(int id) {
+    if (productRepository.findById(id).isPresent()) {
+      return productRepository.findById(id);
+    } else {
+      return null;
+    }
+  }
 }
